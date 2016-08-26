c97b6d182ac6c64ec2d7b59e4eb0d0eb9cf87d78
马少洋 <mayang_email@sina.cn>
2016-08-26 15:38:11 +0800

    Initial Commit

---
+++ WuSeFang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WuSeFang
+//
+//  Created by 马扬 on 16/8/26.
+//  Copyright © 2016年 马扬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
